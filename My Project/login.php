9d82904b589fd4cc23b7a61c53154b73a36aba3e
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2021-07-05 09:38:00 -0600

    Add files via upload

---
+++ My Project/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login---Zetha cars rentals</title>
+    <link href="css/login.css" rel="stylesheet">  
+  <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="center">
+    <h1>login</h1>
+    <form action="" method="post">
+        <div class="txt_field">
+            <input type="text" required>
+            <span></span>
+            <label for="">Username</label>
+        </div>
+        <div class="txt_field">
+            <input type="password" required>
+            <span></span>
+            <label for="">Password</label>
+            </div>
+            <div class="pass"><a href="#">Forget Password?  </a></div>
+            <input type="submit" name="" value="Login">
+            <div class="signup_link">
+                Not a member <a href="#">signup</a>
+            </div>
+        
+    </form>
+
+</div>
+    
+</body>
+</html>
