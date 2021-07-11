a342c6e5f6c28e5ae38c77900104c7d59cb1830f
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2021-07-11 09:29:05 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login---Zetha cars rentals</title>
     <link href="css/login.css" rel="stylesheet">  
+  <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="center">
