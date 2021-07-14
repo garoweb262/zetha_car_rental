8059987c99c2d22a1904db428030ebb46d3f87d8
asmaumahmud <asmaulhusnamahmud005@gmail.com>
2021-07-14 10:13:33 +0100

    adding sql file for login table

---
+++ logintable.sql
@@ -0,0 +1,20 @@
+
+--DATABASE NAME zetha--
+
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `cstm_id` int(100) NOT NULL AUTO_INCREMENT,
+  `username` varchar(250) NOT NULL DEFAULT '',
+  `password` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`cstm_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `mobile_store_basket` (`cstm_id`, `username`, `password`,) VALUES
+(1, 'asmau', 'asmau@@445')
