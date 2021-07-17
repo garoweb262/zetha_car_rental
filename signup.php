e5634167e56a33c2ad26312f37df2a4028f6a1f3
odogwu-malay <85666803+odogwu-malay@users.noreply.github.com>
2021-07-17 01:55:43 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/signup.css" rel="stylesheet">  
+  <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Sign up---Zetha cars rentals</title>
+</head>
+<body>
+<section id="main">
+        <div class="row">
+            <div class="col-md-6">
+                <div id="build">
+                <div class="intro-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                           <h3 class="name">ZETHA CAR RENTALS </h3>
+                        </div>
+                    </div>
+                    <p class="note">No:1, in Renting Vehicles around the nations.</p>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+             <div id="login">
+                 <div class="card">
+                     <div clas=card-header>
+                         
+                       <h3 class="header">signup</h3> 
+                      
+                       </div>
+                       <div class="card-body">
+                        <form action="dashboard.php" method="">
+                            <div class="form-group">
+                                <input type="text" name="username" placeholder="username" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" placeholder="password" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" placeholder="cornfirm password" required>
+                            </div>
+                                <input type="submit" name="submit" value="Sign up">
+                         
+                            <div class="row">
+                            <div class="col-md-12">
+                               <div class="sign_up">A member <a href="login.php">Login</a></div> 
+                            </div>
+                            </div>
+
+                               </div>
+                            </div>
+                        </form>
+                        </div>
+
+                        </div>
+                        </div>
+
+
+                </div>
+            </div>
+          
+
+        </div>
+     
+  </section>
+    
+</body>
+</html>
