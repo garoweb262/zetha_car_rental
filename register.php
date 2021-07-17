e4ebc14767afa2630afde90c9134ebfd57be3664
3418emmy <86464012+3418emmy@users.noreply.github.com>
2021-07-17 08:07:04 +0100

    Add files via upload
    Emmy pushing

---
+++ register.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once "config/config.php";
+ 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $password = $confirm_password = "";
+    $username_err = $password_err = $confirm_password_err = "";
+
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+ 
+        $sql = "SELECT id FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+       
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            $param_username = trim($_POST["username"]);
+            
+            if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+
+            unset($stmt);
+        }
+    }
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+         
+        if($stmt = $pdo->prepare($sql)){
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); 
+            
+            if($stmt->execute()){
+               
+                header("location: login.php");
+            } else{
+                echo "You entered the wrong details, Please try again later.";
+            }
+
+    
+            unset($stmt);
+        }
+    }
+    
+
+    unset($pdo);
+}
+?>
+ 
